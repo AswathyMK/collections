b40feb77651730624fb1f1a9fd101ccef061cecc
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2020-01-08 16:15:06 +0530

    Program to sort ArrayList

--- ArrayList_Example_Sort.java
+++ ArrayList_Example_Sort.java
@@ -1,12 +1,11 @@
 import java.util.*;
 public class ArrayList_Example_Sort 
 {
-
 	public static void main(String[] args) 
 	{
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		ArrayList<Integer> marks=new ArrayList<Integer>();
+		ArrayList<Integer> marks=new ArrayList<Integer>();//creating array list
 		for(int i=0;i<n;i++)
 		{
 			int c=sc.nextInt();
