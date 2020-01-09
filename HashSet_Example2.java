7b86acef65f73fc162b46bbef6a3b6391ac4f806
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2020-01-09 14:28:23 +0530

    Program to copy ArrayList elements to HashSet

---
+++ HashSet_Example2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class HashSet_Example2 
+{
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		ArrayList<Integer> alist=new ArrayList<Integer>();//creating Arraylist
+		//adding elements to ArrayList
+		for(int i=0;i<n;i++)
+		{
+			int s=sc.nextInt();
+			alist.add(s);
+		}
+        HashSet<Integer> set=new HashSet<Integer>();//creating HashSet
+        //Adding elements of ArrayList to HashSet using addAll() method
+        set.addAll(alist);
+        System.out.println(set);
+    }
+
+}
