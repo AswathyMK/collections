e794c04d16809373c27b773eead9ec40c08420bb
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2020-01-10 16:30:12 +0530

    Program using class type in LinkedHashMap

---
+++ LinkedHashMap_Employee_Main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class LinkedHashMap_Employee_Main 
+{
+
+	public static void main(String[] args) 
+	{
+		int n,i;
+		String name;
+		int age;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		Map<Integer,LinkedHashMap_Employee> map=new LinkedHashMap<Integer,LinkedHashMap_Employee>();//creating HashMap of class type LinkedHashMap_Employee
+		LinkedHashMap_Employee[] emp=new LinkedHashMap_Employee[n];//creating array of class object
+		//adding elements to LinkedHashMap
+		for(i=0;i<n;i++)
+		{
+			name=sc.next();
+			age=sc.nextInt();
+			emp[i]=new LinkedHashMap_Employee();
+			emp[i].setAge(age);
+			emp[i].setName(name);
+			map.put(i,emp[i]);
+			
+		}
+		//returning elements from LinkedHashMap
+		for(Map.Entry<Integer,LinkedHashMap_Employee> e:map.entrySet())
+		{
+			LinkedHashMap_Employee obj=e.getValue();
+			System.out.println(e.getKey()+" "+obj.getName()+" "+obj.getAge());
+		}
+		sc.close();
+	}
+
+}
